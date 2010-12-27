e4ad2cdd7e3d9fcccdf01afd94ce3652320a1784
Izzy Cecil <lorr.cecil@gmail.com>
2010-12-27 15:06:46 -0600

    --Inventory fixes--     Inevntory will now loos the stats when loosing items. It also will not accept more than ten items.

--- src/world/elements/Ship/Inventory.java
+++ src/world/elements/Ship/Inventory.java
@@ -9,51 +9,68 @@ import world.elements.Weapon;
 import world.elements.physics.Location;
 import world.elements.physics.Vector;
 
-public class Inventory implements Serializable{
+public class Inventory implements Serializable {
 	/**
 	 * Serial Version ID for Serialization of World objects
 	 */
 	private static final long serialVersionUID = 1;
 	private ArrayList<Item> itemList;
-	private Weapon weapon;	//TODO: make a weapon class.
+	private Weapon weapon; // TODO: make a weapon class.
 	private double shields, speed;
-	
-	public Inventory(){
+
+	public Inventory() {
 		shields = 1;
 		speed = .5;
 		itemList = new ArrayList<Item>();
 		weapon = new Weapon();
 	}
-	
-	public void addItem(Item item){
-		switch(item.getType()){
-		case SHEILDS: shields += item.getPower(); break;
-		case SPEED: speed += item.getPower(); break;
-		default: break;
+
+	public void addItem(Item item) {
+		if (itemList.size() < 10) {
+			switch (item.getType()) {
+			case SHEILDS:
+				shields += item.getPower();
+				break;
+			case SPEED:
+				speed += item.getPower();
+				break;
+			default:
+				break;
+			}
+			itemList.add(item);
 		}
-		itemList.add(item);
 	}
-	
-	public void pickupWeapon(Weapon w){
+
+	public void pickupWeapon(Weapon w) {
 		weapon = w;
 	}
-	
-	public void fire(ArrayList<Bullet> fireList, Location l, Vector v){
+
+	public void fire(ArrayList<Bullet> fireList, Location l, Vector v) {
 		weapon.fire(fireList, l, v);
 	}
-	
-	public void reload(){
+
+	public void reload() {
 		weapon.reload();
 	}
-	
+
 	public ArrayList<Item> getItemList() {
 		return itemList;
 	}
-	
-	public Item dropItem(int i){
+
+	public Item dropItem(int i) {
+		Item item = itemList.get(i);
+		switch (item.getType()) {
+		case SHEILDS:
+			shields -= item.getPower();
+			break;
+		case SPEED:
+			speed -= item.getPower();
+			break;
+		default:
+			break;
+		}
 		return itemList.remove(i);
 	}
-	
 
 	public Weapon getWeapon() {
 		return weapon;
